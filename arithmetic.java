6e3c0c98017e4d32aae6fab63b6ee3921e3bb3d1
Prajwal Biradar <112881872+Prajbi@users.noreply.github.com>
2024-09-29 19:02:54 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,10 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        int p = 105, q = 45;
+        System.out.println(p + q);
+        System.out.println(p - q);
+        System.out.println(p * q);
+        System.out.println(p / q);
+        System.out.println(p % q);
+    }
+}
